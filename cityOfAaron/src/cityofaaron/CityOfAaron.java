3ac5567010df444b63fb97e0759826f8b62cb2b6
Seth Huntley <seth@sethsmbpretina>
2018-05-11 20:18:07 -0500

    updated comment in header

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -2,7 +2,7 @@
  * The main() class file for the cityOfAaron project
  * CIT-260
  * Spring 2018
- *  Team members: Seth Huntley, Bruce Montgomery, Stanley Varner
+ * Team members: Seth Huntley, Bruce Montgomery, Stanley Varner
  */
 package cityofaaron;
 
