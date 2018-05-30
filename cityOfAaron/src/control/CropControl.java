f0e30910df1a0e158ee9ab010216399dde410055
Seth Huntley <seth@sethsmbpretina>
2018-05-30 12:25:10 -0500

    week 6 crop control

---
+++ cityOfAaron/src/control/CropControl.java
@@ -0,0 +1,40 @@
+// The CropControl class - part of the control layer
+// class contains all of the calculation methods for managing the crops
+// Author: Seth Huntley, Bruce Montgomery, Stanley Varner
+// Date last modified: 2018.05.30
+//-------------------------------------------------------------
+package control;
+
+public class CropControl
+{
+    
+// The sellLand method
+// Purpose: To sell land
+// Parameters: the price of land, the number of acres to sell, and
+//    a reference to a CropData object
+// Returns: the number of acres owned after the sale
+// Pre-conditions: acres to sell must be positive
+// and <= acresOwned
+ public static int sellLand(int landPrice, int acresToSell, CropData cropData)
+ {
+     //if acresToSell < 0, return -1
+    if(acresToSell < 0)
+        return -1;
+
+//if acresToSell > acresOwned, return -1
+int owned = cropData.getAcresOwned();
+if(acresToSell > owned)
+    return -1;
+
+//acresOwned = acresOwned - acresToSell
+
+//wheatInStore = wheatInStore + acresToSell * landPrice
+
+//return acresOwned
+
+     
+ }
+
+}
+
+
