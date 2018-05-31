c12c26fe8560189899f926a724019db276811542
brucem <brucem@IDA-WS-044.corp.finfun.com>
2018-05-30 20:17:02 -0600

    5/30  20:15 Reverting back to my original file.

--- cityOfAaron/src/model/CropData.java
+++ cityOfAaron/src/model/CropData.java
@@ -18,7 +18,7 @@ public class CropData implements Serializable {
     private int acresOwned;
     private int cropYield;
     private int wheatInStore;
-    private int numberWhoDIed;
+    private int numberWhoDied;
     private int newPeople;
     private int harvest;
     private int harvestAfterOffering;
@@ -62,11 +62,11 @@ public class CropData implements Serializable {
             public void setwheatInStore(int _wheatInStore) {
                     wheatInStore = _wheatInStore;
             }
-            public int getnumberWhoDIed() {
-                    return numberWhoDIed;
+            public int getnumberWhoDied() {
+                    return numberWhoDied;
             }
-            public void setnumberWhoDIed(int _numberWhoDIed) {
-                    numberWhoDIed = _numberWhoDIed;
+            public void setnumberWhoDied(int _numberWhoDied) {
+                    numberWhoDied = _numberWhoDied;
             }
             public int getnewPeople() {
                     return newPeople;
