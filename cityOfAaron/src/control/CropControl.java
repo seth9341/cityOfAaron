f4922bd0bf0fde92f04381a9b14e21d225bba02c
Stanley Varner <stanmanjr28@gmail.com>
2018-06-03 02:11:30 -0600

    shorter version

--- cityOfAaron/src/control/CropControl.java
+++ cityOfAaron/src/control/CropControl.java
@@ -226,14 +226,11 @@ public static void setOffering(int offering, CropData cropData) {
     
     //check to see if offering is neg or above 100
     //if so print out to get new number
-    if (offering < 0){
-        System.out.println("Please enter a number between 0-100");
-    }
-    if (offering > 100){
+    if (offering < 0 || offering > 100){
         System.out.println("Please enter a number between 0-100");
     }
     //if offering is between 0-100 then save/setoffering
-    else if (offering <= 0 && offering >= 100) {
+    else {
         cropData.setoffering(offering);
     } 
     
