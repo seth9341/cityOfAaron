da4c5ae307ee7b2cdca3510fc00f189ae2bb169d
brucem <brucem@IDA-WS-044.corp.finfun.com>
2018-06-23 20:20:36 -0600

    6/23/2018:  Changed the GameMenuView class to be a subclass of the MenuView class.

--- cityOfAaron/src/view/GameMenuView.java
+++ cityOfAaron/src/view/GameMenuView.java
@@ -7,12 +7,8 @@ package view;
 
 import java.util.Scanner;
 
-public class GameMenuView {
+public class GameMenuView extends MenuView {
     
-    //Create a Scanner object
-    private static Scanner keyboard = new Scanner(System.in);
-    private String theMenu;
-    private int max;
         
 // The GameMenuView constructor
 // Purpose: Initialize the game menu data
@@ -21,7 +17,7 @@ public class GameMenuView {
 // ===================================
 public GameMenuView()
 {
-        theMenu = "\n" +
+        super("\n" +
                    "**********************************\n" +
                    "* GAME MENU                      *\n" +
                    "**********************************\n" +
@@ -29,71 +25,16 @@ public GameMenuView()
                    " 2 - View/Print a list\n" +
                    " 3 - Move to a new location\n" +
                    " 4 - Manage the Crops\n" +
-                   " 5 - Return to the Main menu\n";
+                   " 5 - Return to the Main menu\n",
         
-        max = 5;
+            5);
 }
-
- // The displayMenuView method
-    // Purpose: displays the menu, gets the user's input, and does the 
-    //               selected action
-    // Parameters: none
-    // Returns: none
-    // =========================================================    
-public void displayMenuView()
-{
-    int menuOption;
-    do
-    {   
-     // Display the menu
-        System.out.println(theMenu);
-
-     // Prompt the user and get the user’s input
-        menuOption = getMenuOption();
-
-     // Perform the desired action
-        doAction(menuOption);
-
-     // Determine and display the next view
-    } while (menuOption != max);
-
-
-}
-    // The getMenuOption method
-    // Purpose: gets the user's input
-    // Parameters: none
-    // Returns: integer - the option selected
-    // ===================================       
-    public int getMenuOption()
-    {
-        // declare a variable to hold user’s input
-        int userInput;
-        
-        //begin loop         
-        do
-        {
-        // get user input from the keyboard
-            userInput = keyboard.nextInt();
-
-        // if it is not a valid value, output an error message
-            if(userInput < 1 || userInput > max)
-            {
-                System.out.println("\noption must be between 1 and " + max);
-            }
-
-        // loop back to the top if input was not valid
-        } while(userInput < 1 || userInput > max);
-
-        // return the value input by the user
-        return userInput;
-        
-    }  
     // The doAction method
     // Purpose: performs the selected action
     // Parameters: none
     // Returns: none
     // ===================================       
-    public void doAction(int option)
+    @Override public void doAction(int option)
     {
         switch(option)
         {
