b5abb99f74e84e7ce6f3d63eb1568671cf57c151
Seth Huntley <seth@sethsmbpretina>
2018-07-06 13:52:36 -0500

    FeedPeopleException

--- cityOfAaron/src/exceptions/FeedPeopleException.java
+++ cityOfAaron/src/exceptions/FeedPeopleException.java
@@ -9,6 +9,20 @@ package exceptions;
  *
  * @author seth
  */
-public class FeedPeopleException {
+public class FeedPeopleException  extends Exception {
     
+    //Default contstructor
+    //Purpose: Initializes data members to default values
+    //Parameters: none
+    //Returns: non
+    public FeedPeopleException(){}
+    
+    //Parameterized constructor
+    //Purpose: Initializes data members to value passed as a parameter
+    //Parameters: A string containing the error message
+    //Returns: non
+    public FeedPeopleException(String string)
+    {
+        super(string);
+    }
 }
