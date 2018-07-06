992fb417bdeb6ef874c1ddc7e7f25ac7b8128260
Seth Huntley <seth@sethsmbpretina>
2018-07-06 14:00:54 -0500

    added FeedPeopleException to FeedPeople in cropcontrol

--- cityOfAaron/src/control/CropControl.java
+++ cityOfAaron/src/control/CropControl.java
@@ -1,7 +1,7 @@
 // The CropControl class - part of the control layer
 // class contains all of the calculation methods for managing the crops
 // Author: Seth Huntley, Bruce Montgomery, Stanley Varner
-// Date last modified: 2018.05.30
+// Date last modified: 2018.07.06
 //-------------------------------------------------------------
 
 
